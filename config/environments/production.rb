ac6118cac2400719c3542cc07216352d24c7dde6
Nick Barnwell <nb@ul.io>
2014-05-06 22:24:01 -0700

    Fix Prod.rb syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     domain: 'heroku.com',
-    authentication: :plain
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD']
