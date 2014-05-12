d47f4d8786b23de08d27b1abc80502da2545efc9
Nick Barnwell <nb@ul.io>
2014-05-11 21:25:58 -0700

    Use Paperclip with S3 in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,15 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD']
   }
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
