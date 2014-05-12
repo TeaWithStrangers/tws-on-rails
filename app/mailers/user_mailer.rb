c2ac36a0719764f680b80887dcb08ef256113f7a
Nick Barnwell <nb@ul.io>
2014-05-11 21:22:15 -0700

    Mailer stuffs (actually delivering now)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,25 +3,25 @@ class UserMailer < ActionMailer::Base
 
   def user_registration(user, password)
     @user = user; @password=password;
-    mail to: @user.email, subject: 'Welcome to Tea With Strangers!'
+    mail(to: @user.email, subject: 'Welcome to Tea With Strangers!').deliver!
   end
 
   def tea_time_registration(attendance)
     @attendance = attendance
-    mail to: @user.email, subject: "See You at Tea Time!"
+    mail(to: @user.email, subject: "See You at Tea Time!").deliver!
   end
 
   def tea_time_cancellation(tea_time)
     @tea_time = @tea_time
     tea_time.attendances.each do |att|
       @user = att.user 
-      mail to: @user.email, subject: "Uh-oh :( Tea Time Canceled"
+      mail(to: @user.email, subject: "Uh-oh :( Tea Time Canceled").deliver!
     end
   end
 
   def tea_time_flake(attendance)
     @user = attendance.user
     @tea_time = attendance.tea_time
-    mail to: @user.email, subject: "Sorry You Couldn't Make It :("
+    mail(to: @user.email, subject: "Sorry You Couldn't Make It :(").deliver!
   end
 end
