a04161fdf98ae92e11a75dd54e84df7018930dc4
Nick Barnwell <nb@ul.io>
2014-05-13 20:23:44 -0700

    Cities grouped by host count in static index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,16 +1,19 @@
 class StaticController < ApplicationController
   def index
     @cities = City.all
-    @cities_by_hosts = @cities.group_by { |x| 
-      if x.hosts.count == 1
-        :single_host
-      elsif x.hosts.count > 1
-        :many_hosts
-      else
-        :no_hosts
-      end
-    }
-    pp @cities_by_hosts
+    @cities_by_host  = 
+      #TODO: I had standards once
+      {single_host: [],
+       many_hosts: [],
+       no_hosts: []}.merge(@cities.group_by { |x| 
+         if x.hosts.count == 1
+           :single_host
+         elsif x.hosts.count > 1
+           :many_hosts
+         else
+           :no_hosts
+         end
+       })
   end
 
   def stories
