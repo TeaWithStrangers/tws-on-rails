36164001330cf5d0eb06c10e136697e3f01ba18a
Nick Barnwell <nb@ul.io>
2014-05-13 20:14:53 -0700

    Signup form styles, cities in static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,16 @@
 class StaticController < ApplicationController
   def index
+    @cities = City.all
+    @cities_by_hosts = @cities.group_by { |x| 
+      if x.hosts.count == 1
+        :single_host
+      elsif x.hosts.count > 1
+        :many_hosts
+      else
+        :no_hosts
+      end
+    }
+    pp @cities_by_hosts
   end
 
   def stories
