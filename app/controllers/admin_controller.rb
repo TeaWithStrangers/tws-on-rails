427fa4e2e9b129d13bec6661f9b49e1d8c95d141
Nick Barnwell <nb@ul.io>
2014-05-16 09:43:56 -0700

    Admin find feature

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,22 @@
+class AdminController < ApplicationController
+  before_filter :authorized?
+  def find
+  end
+
+  def ghost
+    user = User.find_by(email: params[:email])
+    if user
+      sign_in(:user, user)
+      redirect_to profile_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+    def authorized?
+      unless current_user.admin?
+        redirect_to root_url, :notify => "You're not allowed in here!"
+      end
+    end
+end
