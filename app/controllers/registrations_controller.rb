ba07d9aeb2cd16afd4e998a0302e057e162cab62
Nick Barnwell <nb@ul.io>
2014-05-15 20:19:25 -0700

    Add a home city to users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
+    city = City.find_by(id: params[:city])
     if params[:user][:autogen]
       generated_password = Devise.friendly_token.first(8)
       user = User.new(name: params[:user][:name], 
                       email: params[:user][:email],
-                      password: generated_password)
+                      password: generated_password,
+                      home_city: city)
       if user.save
         UserMailer.user_registration(user, generated_password)
         sign_in(:user, user)
         message = "All registered! Check your email for your password."
-        if params[:city]
-          redirect_to City.find(params[:city]), message: message
+        if city
+          redirect_to , message: message
         else
           redirect_to root_path, message: message
         end
