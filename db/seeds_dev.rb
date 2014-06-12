adb4faec944ae77f66d95495c260ff90157042e5
Nick Barnwell <nb@ul.io>
2014-06-12 08:16:52 -0700

    Add Host, Admin Users to Seed

--- db/seeds_dev.rb
+++ db/seeds_dev.rb
@@ -3,11 +3,32 @@ puts "Creating a city"
 sf = City.create( name: 'San Francisco', city_code: 'SF', timezone: 'Pacific Time (US & Canada)')
 
 # Create a user
-puts "Creating a user"
-User.create(
+puts "Creating users"
+user = User.create(
   name: 'Foo Bar',
-  email: 'admin@admin.com',
+  email: 'user@tws-int.com',
   password: 'secret1234',
   password_confirmation: 'secret1234',
   home_city_id: sf.id
 )
+
+host = User.create(
+  name: 'Bar Baz',
+  email: 'host@tws-int.com',
+  password: 'secret1234',
+  password_confirmation: 'secret1234',
+  home_city_id: sf.id
+)
+
+admin = User.create(
+  name: 'Qux Baz',
+  email: 'admin@tws-int.com',
+  password: 'secret1234',
+  password_confirmation: 'secret1234',
+  home_city_id: sf.id
+)
+
+
+host.roles << Role.find_by(name: 'Host')
+admin.roles << Role.find_by(name: 'Admin')
+[host,admin].map(&:save)
