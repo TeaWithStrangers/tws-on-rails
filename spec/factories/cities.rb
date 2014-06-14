29b5b3daa38e223ae4123a27f7214ccb916212bb
Nick Barnwell <nb@ul.io>
2014-06-14 14:29:32 -0700

    City and user basic factories

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -2,5 +2,17 @@
 
 FactoryGirl.define do
   factory :city do
+    sequence(:name) {|n| "City #{n}" }
+    sequence(:city_code) {|n| "city-#{n}" }
+    brew_status "fully_brewed"
+    timezone "Pacific Time (US & Canada)"
+    tagline "Tagline"
+    description "Something bueler bueler bueler lorem"
+
+    City.brew_statuses.each do |k,v|
+      trait k.to_sym do
+        brew_status k
+      end
+    end
   end
 end
