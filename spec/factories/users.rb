ffed5fe7723db9049e17b50b36882d26bcd86725
Nick Barnwell <nb@ul.io>
2014-06-14 14:29:32 -0700

    City and user basic factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,20 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :user do
+    name "Joe User"
+    sequence(:email) { |n| "dummy#{n}@teawithstrangers.com" }
+    password "password"
+
+    [:host, :admin].each do |t| #YOLO
+      trait t do
+        name "Joe #{t.capitalize}"
+
+        after :build do |u|
+          u.roles << Role.find_by(name: t.capitalize)
+        end
+      end
+    end
   end
 end
