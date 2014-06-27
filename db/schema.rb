3d2e1fe5d16537d848f75a8ae1c198d196a94441
Mehul Kar <mehul.kar@gmail.com>
2014-06-27 01:33:50 -0700

    Add twitter column and some validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627041010) do
+ActiveRecord::Schema.define(version: 20140627083148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20140627041010) do
     t.text     "tagline"
     t.integer  "home_city_id"
     t.string   "facebook"
+    t.string   "twitter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
