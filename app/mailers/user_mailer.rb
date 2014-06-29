598e38b35e08280a77d148860510dcc8d5991e8c
Nick Barnwell <nb@ul.io>
2014-06-28 22:28:09 -0700

    User Registration mails and logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,14 @@
-class TeaTimeMailer < ActionMailer::Base
+class UserMailer < ActionMailer::Base
   default from: "\"Tea With Strangers\" <sayhi@teawithstrangers.com>"
 
-  def user_registration(user, password)
+  def registration(user, password)
     @user = user; @password=password;
+    template = @user.home_city.tea_times.future_until(2.weeks.from_now).blank? ? 
+      'registration_no_tea' : 'registration'
     mail(from: "\"Ankit at Tea With Strangers\" <ankit@teawithstrangers.com>",
          to: @user.email, 
-         subject: 'High fives from Tea With Strangers!').
+         subject: 'High fives from Tea With Strangers!',
+        template_name: template).
          deliver!
   end
 end
