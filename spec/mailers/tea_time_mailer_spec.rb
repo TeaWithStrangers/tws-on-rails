df9a1908adf1b539f5e96aae88f048bac0be4aa9
Nick Barnwell <nb@ul.io>
2014-07-05 15:54:50 -0700

    Reminder tests

--- spec/mailers/tea_time_mailer_spec.rb
+++ spec/mailers/tea_time_mailer_spec.rb
@@ -4,6 +4,7 @@ describe TeaTimeMailer do
   before(:all) do
     Delayed::Worker.delay_jobs = false
     @tt = build(:tea_time)
+    @attendance = build(:attendance, tea_time: @tt)
   end
 
 
@@ -27,5 +28,33 @@ describe TeaTimeMailer do
     it 'assigns @name' do
       expect(mail.body.encoded).to match(@tt.host.name)
     end
+  end
+
+  describe '.reminder' do
+    context 'flaked attendees' do
+      it "shouldn't send a reminder for a flake" do
+        @attendance.status = :flake
+        TeaTimeMailer.reminder(@attendance, :same_day)
+        expect(ActionMailer::Base.deliveries.size).to eq(0)
+      end
+    end
+    
+    let(:mail) {
+      TeaTimeMailer.reminder(@attendance, :same_day)
+    }
+
+    it 'should come from the host' do
+      expect(mail.from).to eq([@tt.host.email])
+    end
+
+    it 'should go to attendee' do
+      expect(mail.to).to eq([@attendance.user.email])
+    end
+
+    it 'should contain tea time and host info' do
+      expect(mail.body.encoded).to match(@tt.friendly_time)
+      expect(mail.body.encoded).to match(@tt.host.name)
+    end
+
   end
 end
