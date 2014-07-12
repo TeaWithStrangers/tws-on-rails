9ed3e75221e1049f6d090b814c35905a9829ea1a
Nick Barnwell <nb@ul.io>
2014-07-12 16:54:12 -0700

    Create attendance mailer

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,7 +6,7 @@ class Attendance < ActiveRecord::Base
   validates_uniqueness_of :user_id, scope: :tea_time_id
 
   before_create :check_capacity
-  after_create :queue_reminder_mails
+  after_create :queue_reminder_mails, :queue_ethos_mails
 
   def todo?
     pending?
@@ -14,14 +14,17 @@ class Attendance < ActiveRecord::Base
 
   def flake!
     update_attribute(:status, :flake)
-    TeaTimeMailer.delay.flake(self.id)
+    AttendanceMailer.delay.flake(self.id)
+  end
+
+  def queue_ethos_mails
+    TeaTimeMailer.delay(run_at: Time.now + 1.hour).ethos(self.user.id)
   end
 
   def queue_reminder_mails
     st = self.tea_time.start_time
-    TeaTimeMailer.delay(run_at: Time.now + 1.hour).ethos(user.id)
-    TeaTimeMailer.delay(run_at: st - 2.days).reminder(id, :two_day)
-    TeaTimeMailer.delay(run_at: st - 12.hours).reminder(id, :same_day)
+    AttendanceMailer.delay(run_at: st - 2.days).reminder(self.id, :two_day)
+    AttendanceMailer.delay(run_at: st - 12.hours).reminder(self.id, :same_day)
   end
 
 
