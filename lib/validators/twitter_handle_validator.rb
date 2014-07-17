7608f38db3af54a87904a652bd95f8502d68de43
Mehul Kar <mehul.kar@gmail.com>
2014-07-16 18:45:00 -0700

    Don't validate twitter if it is an empty string

--- lib/validators/twitter_handle_validator.rb
+++ lib/validators/twitter_handle_validator.rb
@@ -1,7 +1,7 @@
 class Validators::TwitterHandleValidator < ActiveModel::Validator
   def validate(record)
     twitter_handle = record.twitter
-    if twitter_handle
+    if twitter_handle.present? # checks empty strings
       unless twitter_handle.match(/^([A-Za-z0-9_]+{1,15}$)/)
         record.errors[:twitter] << 'not a valid twitter handle'
       end
