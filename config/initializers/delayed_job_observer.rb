40788bf9c16c1eddedcec24e935c799f2d5698b1
Nick Barnwell <nb@ul.io>
2014-07-20 14:29:08 -0700

    Only load DJ observer in dev and test

--- config/initializers/delayed_job_observer.rb
+++ config/initializers/delayed_job_observer.rb
@@ -1,40 +1,41 @@
-# Thanks to @dblock and @artsy!
+if !Rails.env.production?
+  # Thanks to @dblock and @artsy!
+  class DelayedJobObserver < ActiveRecord::Observer
+    observe Delayed::Job
 
-class DelayedJobObserver < ActiveRecord::Observer
-  observe Delayed::Job
+    class << self
+      attr_accessor :total_processed
+      attr_accessor :total_errors
+      attr_accessor :enabled
 
-  class << self
-    attr_accessor :total_processed
-    attr_accessor :total_errors
-    attr_accessor :enabled
+      def enabled?
+        !! DelayedJobObserver.enabled
+      end
 
-    def enabled?
-      !! DelayedJobObserver.enabled
+      def enable!
+        DelayedJobObserver.enabled = true
+      end
+
+      def disable!
+        DelayedJobObserver.enabled = false
+      end
+
+      def reset!
+        DelayedJobObserver.total_processed = 0
+        DelayedJobObserver.total_errors = 0
+        DelayedJobObserver.enable!
+      end
     end
 
-    def enable!
-      DelayedJobObserver.enabled = true
-    end
-
-    def disable!
-      DelayedJobObserver.enabled = false
-    end
-
-    def reset!
-      DelayedJobObserver.total_processed = 0
-      DelayedJobObserver.total_errors = 0
-      DelayedJobObserver.enable!
+    def after_create(delayed_job)
+      begin
+        delayed_job.invoke_job if DelayedJobObserver.enabled?
+      rescue
+        DelayedJobObserver.total_errors += 1
+      end
+      DelayedJobObserver.total_processed += 1
     end
   end
 
-  def after_create(delayed_job)
-    begin
-      delayed_job.invoke_job if DelayedJobObserver.enabled?
-    rescue
-      DelayedJobObserver.total_errors += 1
-    end
-    DelayedJobObserver.total_processed += 1
-  end
+  DelayedJobObserver.reset!
 end
-
-DelayedJobObserver.reset!
