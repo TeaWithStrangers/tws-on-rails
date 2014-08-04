93d4d798ea9cfe9d8922fd3302a0de8fc72036da
Nick Barnwell <nb@ul.io>
2014-08-04 12:27:26 -0700

    Activate paranoia gem on TTs and Attendances

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20140804192355) do
     t.integer  "home_city_id"
     t.string   "facebook"
     t.string   "twitter"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
