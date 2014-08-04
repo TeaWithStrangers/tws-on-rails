f9887af773618c8536946c5e27f3e4dd8adcc5f6
Nick Barnwell <nb@ul.io>
2014-08-04 12:27:10 -0700

    Add paranoia gem and migrations

---
+++ db/migrate/20140804192355_add_delete_at_to_tea_times_and_attendances.rb
@@ -0,0 +1,6 @@
+class AddDeleteAtToTeaTimesAndAttendances < ActiveRecord::Migration
+  def change
+    add_column :tea_times, :deleted_at, :datetime, index: true
+    add_column :attendances, :deleted_at, :datetime, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628183917) do
+ActiveRecord::Schema.define(version: 20140804192355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140628183917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "reason"
+    t.datetime "deleted_at"
   end
 
   create_table "cities", force: true do |t|
@@ -86,6 +87,7 @@ ActiveRecord::Schema.define(version: 20140628183917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.datetime "deleted_at"
   end
 
   create_table "users", force: true do |t|
