5b3413873b22bcdd0e2947c37d76bc9a6f39ef91
Nick Barnwell <nb@ul.io>
2014-08-10 10:29:08 -0700

    Mass mailer and Redcarpet helper

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,9 @@
+module MarkdownHelper
+  MD = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                               :autolink => true,
+                               :space_after_headers => true,
+                               :no_intra_emphasis => true)
+  def markdown(md)
+    MD.render(md)
+  end
+end
