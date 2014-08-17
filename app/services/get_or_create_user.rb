37aa3235f2996c4eed98a43476b95474c2232a97
Nick Barnwell <nb@ul.io>
2014-08-16 20:16:59 -0700

    Spec for GetOrCreateUser

--- app/services/get_or_create_user.rb
+++ app/services/get_or_create_user.rb
@@ -1,4 +1,6 @@
 class GetOrCreateUser
+  # Returns a hash with keys :user and :new_user?
+  # :user => User, :new_user? => Boolean
   def self.call(params, home_city)
     #Remove autogen directive from parameters if it exists
     params.delete(:autogen)
@@ -10,11 +12,12 @@ class GetOrCreateUser
     if user
       val[:new_user?] = false
     else
-      generated_password = Devise.friendly_token.first(8)
+      password = Devise.friendly_token.first(8)
       user = User.create(params.merge({home_city: home_city,
-                                             password: generated_password}).symbolize_keys)
-      UserMailer.delay.registration(user, generated_password)
+                                       password: password}).symbolize_keys)
+      UserMailer.delay.registration(user, password)
     end
+
     return val.merge(user: user)
   end
 end
