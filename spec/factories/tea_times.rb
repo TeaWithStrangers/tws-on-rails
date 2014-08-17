be20477e802224453e2d25d54b6c9766fdfb09f7
Nick Barnwell <nb@ul.io>
2014-08-16 22:03:20 -0700

    Update tea time factories and corresponding tests

--- spec/factories/tea_times.rb
+++ spec/factories/tea_times.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     association :host, factory: [:user, :host]
     start_time DateTime.now.midnight + 2.days + 12.hours
     duration 2
+    ignore { attendee_count 0 }
 
     trait :past do
       start_time DateTime.now.midnight - 2.days
@@ -13,20 +14,16 @@ FactoryGirl.define do
       followup_status :cancelled
     end
 
-    factory :tt_with_attendees do
-      ignore do
-        attendee_count 3
-      end
+    trait :attended do
+      ignore { attendee_count 3 }
+    end
 
-      trait :full do
-        ignore do
-          attendee_count 5
-        end
-      end
+    trait :full do
+      ignore { attendee_count 5 }
+    end
 
-      after(:create) do |tt, evaluator|
-        create_list(:attendance, evaluator.attendee_count, tea_time: tt)
-      end
+    after(:create) do |tt, evaluator|
+      create_list(:attendance, evaluator.attendee_count, tea_time: tt)
     end
   end
 end
