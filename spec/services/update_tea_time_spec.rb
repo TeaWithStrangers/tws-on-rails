9b13d3932c270e6036c0572de27c1ae9df6feb11
Nick Barnwell <nb@ul.io>
2014-09-21 15:09:42 -0700

    Tests for host changing tea time

---
+++ spec/services/update_tea_time_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper.rb'
+
+describe UpdateTeaTime do
+  let(:params) { {location: 'Somewhere Else'} }
+  let(:new_host_params) { {user_id: create(:user, :host).id } }
+  let(:tt) { create(:tea_time, :attended) }
+
+  describe '#call' do
+    it 'should update the tea time' do
+      UpdateTeaTime.call(tt, params)
+      expect(tt.location).to eq params[:location]
+      expect(tt.persisted?).to eq true
+    end
+
+    it 'should change the host and fire a notification' do
+      UpdateTeaTime.call(tt, new_host_params)
+      expect(TeaTime.find(tt.id).host).to eq User.find(new_host_params[:user_id])
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
+  end
+  
+end
