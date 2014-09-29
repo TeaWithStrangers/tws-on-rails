2962bb7a62d895d0ec414138d51004319d34b6c5
Mehul Kar <mehul.kar@gmail.com>
2014-09-29 07:24:03 -0700

    Attendance update route can now take a reason from params. Updates #78

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -11,8 +11,11 @@ class AttendanceController < ApplicationController
   # It should probably look at params to see what the update
   # request is trying to do.
   def update
+    @tea_time = params[:id]
+    @attendance = Attendance.find_by(tea_time: @tea_time, user: current_user)
+
     respond_to do |format|
-      if @attendance.flake!({reason: params[:attendance][:reason]})
+      if @attendance.flake!({reason: params[:reason]})
         format.html { redirect_to profile_path, notice: 'Your spot is now open for someone else!' }
         format.json { render :show, status: :created, location: @tea_time }
       else
