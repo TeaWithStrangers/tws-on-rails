ca30ebd0aaff175bce741635570ec697387220a9
Nick Barnwell <nb@ul.io>
2014-09-30 23:34:11 -0700

    User Mailer spec and fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,15 +5,19 @@ class UserMailer < ActionMailer::Base
   default from: "\"Tea With Strangers\" <sayhi@teawithstrangers.com>"
 
   def registration(user, password)
-    @user = user; @password=password;
+    sendgrid_category "User Registration"
+
+    @user = user; @password = password;
+
+    # We send a different mail if the user has registered for a tea time
     template = @user.home_city.tea_times.future_until(2.weeks.from_now).empty? ?
       'registration_no_tea' : 'registration'
+
     mail(from: "\"Ankit at Tea With Strangers\" <ankit@teawithstrangers.com>",
          to: @user.email, 
-         subject: "Thanks for being awesome, #{@user.name}!",
-         template_name: template) do |format|
-           format.text
-           format.html
+         subject: "Thanks for being awesome, #{@user.name}!") do |format|
+           format.text { render template }
+           format.html { render template }
          end
   end
 end
