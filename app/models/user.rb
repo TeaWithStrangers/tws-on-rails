b5e64858c7ee7ceeb1045e2adcfa14d075eb1eab
Nick Barnwell <nb@ul.io>
2014-10-09 22:04:44 -0700

    Confirmable user model
    * Database migration with confirmable fields
    * Refactors out some user role related things incl. confirmable stuff into a
      mixin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  include Usable
+  acts_as_paranoid
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
