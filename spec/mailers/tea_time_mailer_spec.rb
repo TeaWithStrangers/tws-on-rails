1573d589ef4e54ca59d873822700cc6936ef5507
Nick Barnwell <nb@ul.io>
2014-12-21 12:28:39 -0800

    Some preliminary tests. Hacky. Temporary

--- spec/mailers/tea_time_mailer_spec.rb
+++ spec/mailers/tea_time_mailer_spec.rb
@@ -29,17 +29,33 @@ describe TeaTimeMailer do
 
   describe '#host_changed' do
     let!(:mail) { TeaTimeMailer.host_changed(@tt, create(:user, :host)).deliver }
+
+    it 'gets delivered without error' do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
   end
 
   describe '#followup' do
     let!(:mail) { TeaTimeMailer.followup(@tt).deliver }
+
+    it 'gets delivered without error' do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
   end
 
   describe '#cancellation' do
     let!(:mail) { TeaTimeMailer.cancellation(@tt, @tt.attendances.sample).deliver }
+
+    it 'gets delivered without error' do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
   end
 
   describe '#ethos' do
-    let!(:mail) { TeaTimeMailer.ethos(@tt).deliver }
+    let!(:mail) { TeaTimeMailer.ethos(@tt.host).deliver }
+
+    it 'gets delivered without error' do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
   end
 end
