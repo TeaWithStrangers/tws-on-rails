9735bb8568d11b370db2c85225c11ed009599ea8
Ankit <a.shah811@gmail.com>
2015-01-10 20:11:39 -0800

    fixed tea time location width and name font size for attendance

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -11,30 +11,6 @@ class AttendanceController < ApplicationController
 
   def mark
     @tea_time = TeaTime.find(params[:id])
-<<<<<<< HEAD
-
-    case params[:marking]
-    when 'email'
-      if params[:email_sent] == 'true'
-        if @tea_time.advance_state!
-          redirect_to host_tasks_path,
-            notice: 'All done!'
-        else
-          redirect_to :back, alert: "That didn't work. Try again?"
-        end
-      end
-    when 'attendance'
-      tea_time_params = params.fetch(:tea_time, {}).permit!
-      if @tea_time.update!(tea_time_params) && @tea_time.advance_state!
-        redirect_to host_tasks_path,
-          notice: 'Thanks for taking attendance! Now send an email to your attendees :)'
-      else
-        redirect_to :back, alert: 'Uh-oh. Something went wrong. Care to try again?'
-      end
-    when 'edit_attendance'
-      @tea_time.update(followup_status: "pending")
-      redirect_to :back, notice: "OK, care to mark again?"
-=======
     respond_to do |format|
       case params[:marking]
       when 'email'
@@ -62,7 +38,6 @@ class AttendanceController < ApplicationController
         @tea_time.update(followup_status: "pending")
         format.html { redirect_to :back, notice: "OK, care to mark again?" }
       end
->>>>>>> 45f6b0d84cd889ab6a9cbf70abb0597700f89795
     end
   end
 
