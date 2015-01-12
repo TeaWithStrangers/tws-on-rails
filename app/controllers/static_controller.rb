0459b31d46bfb9fb15d0284c27a0a0232ef50e73
Nick Barnwell <nb@ul.io>
2015-01-11 21:18:52 -0800

    No hosts; all one-third

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,14 +3,12 @@ class StaticController < ApplicationController
     @cities = City.visible
     @cities_by_host  = Hash.new([]).merge(
       @cities.group_by { |x|
-         if (x.fully_brewed? || x.hosts.count > 1)
-           :many_hosts
-         elsif (x.warming_up? || x.hosts.count == 1)
-           :single_host
-         else
-           :no_hosts
-         end
-       })
+        if (x.fully_brewed? || x.warming_up?)
+          :hosts
+        else
+          :no_hosts
+        end
+      })
   end
 
   def stories
