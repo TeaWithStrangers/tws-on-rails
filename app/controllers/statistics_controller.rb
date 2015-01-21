4ae95dc809f4b206e735b9f65d4da578a8109c98
Nick Barnwell <nb@ul.io>
2015-01-20 22:42:24 -0800

    Refine all the statistics controller queries

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,65 +1,52 @@
-class StatisticsController < ActiveRecord::BaseController
+class StatisticsController < ApplicationController
   respond_to :json
 
   def active_hosts
-    res = City.all.map do |city|
+    data = City.all.map do |city|
       hosts = city.hosts
       active_hosts = []
       hosts.each do |host|
-        if host.tea_times.after(Time.now.end_of_week - 2.weeks).before(Time.now.end_of_week).count
+        if host.tea_times.before(Time.now.end_of_week).after(Time.now.end_of_week - 2.weeks).count > 0
           active_hosts << host
         end
       end
 
       {
-        stat: "#{city.name}",
-        value: sprintf("%0.0f%", ((active_hosts.count == 0 or hosts.count == 0) ? 0 : (active_hosts.count.to_f / hosts.count)*100))
+        active_hosts: active_hosts.map {|u| u.as_json(only: [:name, :id]) },
+        inactive_hosts: (hosts - active_hosts).map {|u| u.as_json(only: [:name, :id]) },
+        active_hosts_count: active_hosts.count,
+        total_host_count: hosts.count,
+        city: city_serializer(city)
       }
     end
-
-    render res
+    render json: data, root: false
   end
 
   def hosts_by_city
-    @data = []
-
-    @cities = City.all.order('id').all
-
-    @cities.map {|city|
-      @data << {
-        'label' => city.name,
-        'value' => city.hosts.count
-      }
+    #TODO: Once we merge in user role bitmasks we can simplify this into a
+    #single query that just joins cities and users together on home_city_id
+    #where user_roles & x > 0 
+    data = User.hosts.group(:home_city_id).count.map { |city_id, count| 
+      {
+        city: city_serializer(City.find(city_id)),
+        total_host_count: count 
+      } 
     }
 
-    @data.each_with_index { |item, index|
-      item.merge! @@graph_color_map[index]
-      @data[index] = item
-    }
-
-    apiResponse = {'response' => @data}
-
-    render apiResponse
+    render json: data, root: false
   end
 
   def teatimes_by_city
-    @data = []
-
-    @cities = City.all.order('id').all
-
-    @cities.map {|city|
-      @data << {
-        'label' => city.name,
-        'value' => city.tea_times.count
-      }
-    }
-
-    @data.each_with_index { |item, index|
-      item.merge! @@graph_color_map[index]
-      @data[index] = item
-    }
-
-    apiResponse = {'response' => @data}
-
-    render apiResponse
+    records = City.select('cities.name, cities.id, count(*)').
+      joins('inner join tea_times on
+            tea_times.city_id=cities.id').
+      group('cities.name, cities.id order by count(tea_times.id) desc')
+    data = records.map { |c| {city: city_serializer(c) , count: c.count } }
+    render json: data, root: false
   end
+  
+  private
+  def city_serializer(city)
+    city.as_json(only: [:name, :id])
+  end
+end
