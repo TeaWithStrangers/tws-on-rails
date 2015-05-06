f989977b01e690c67f2a092f02ef0aad803e9399
dan <dlafeir@umich.edu>
2015-05-05 21:05:48 -0700

    adding cities page

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,5 +1,24 @@
 var danCallThisFunction = function() {
-  console.log('working?');
+
+  var listOfActiveCities = [];
+  var listOfUpcomingCities = [];
+
+  $.get('http://localhost:3000/api/v1/cities', function(data){
+    $.each(data.cities, function(index, city){
+      console.log(city);
+
+      var cityDiv = '<div class="city"><img class="city-image" src="'+city.header_bg+'"/><div class="city-name">'+city.name+'</div></div>';
+      if(city.brew_status === "fully_brewed"){
+        listOfActiveCities.push(cityDiv);
+      }
+      else{
+        listOfUpcomingCities.push(cityDiv);
+      }
+    });
+    $('.current-cities-container').html(listOfActiveCities.join(' '));
+    $('.upcoming-cities-container').html(listOfUpcomingCities.join(' '));
+  });
+
 };
 
 
