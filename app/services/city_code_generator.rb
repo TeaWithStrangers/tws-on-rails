88cd61271f466693f4c920a081e8db2f0fad816a
mehulkar <mehul.kar@gmail.com>
2015-05-09 19:34:12 -0700

    Use SecureRandom.hex to generate random city code

--- app/services/city_code_generator.rb
+++ app/services/city_code_generator.rb
@@ -3,6 +3,6 @@
 # and use the name of the city to generate a code
 class CityCodeGenerator
   def self.generate
-    (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+    SecureRandom.hex(4)
   end
 end
