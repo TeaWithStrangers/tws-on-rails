af1fb2353c5a70cef7b18876ea7d67fba1582fa0
Mehul Kar <mehul.kar@apple.com>
2015-05-09 19:31:30 -0700

    Restrict POST /api/v1/cities to signed in users

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -3,16 +3,19 @@ class Api::V1::CitiesController < ApplicationController
     render json: City.all
   end
 
-  # TODO must be signed in
   def create
-    city              = City.new(params.require(:city).permit(:name))
-    city.timezone     = "Pacific Time (US & Canada)" if !city.timezone
-    city.city_code    = CityCodeGenerator.generate if !city.city_code
-    city.brew_status  = "unapproved"
-    if city.save
-      render json: city
+    if user_signed_in?
+      city              = City.new(params.require(:city).permit(:name))
+      city.timezone     = "Pacific Time (US & Canada)" if !city.timezone
+      city.city_code    = CityCodeGenerator.generate if !city.city_code
+      city.brew_status  = "unapproved"
+      if city.save
+        render json: city
+      else
+        render json: city.errors, status: 422
+      end
     else
-      render json: city.errors, status: 422
+      render json: :unauthenticated, status: 401
     end
   end
 end
