4c125dd04d16d5eb021ba0f57eb606d4eec60afc
Mehul Kar <mehul.kar@apple.com>
2015-05-10 22:39:55 -0700

    Assign nickname from FB info to pass user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ActiveRecord::Base
       user.email    = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name     = auth.info.name
+      user.nickname = auth.info.first_name
       user.avatar   = auth.info.image
     end
   end
