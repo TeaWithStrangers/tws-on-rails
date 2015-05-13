af6764b3b5455eb60eec1afdd7617d23952c365f
Nick Barnwell <nb@ul.io>
2015-05-13 00:07:02 -0700

    Progress bar stubs and required resources

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,46 +1,49 @@
 var onCitiesIndexLoad = function() {
+  if(window.location.pathname === '/cities/' || 
+     window.location.pathname === '/cities') {
 
-  var listOfActiveCities = [];
-  var listOfUpcomingCities = [];
+    var listOfActiveCities = [];
+    var listOfUpcomingCities = [];
 
-  var cityLink = function(city) {
-    return '<a href="/' + city.city_code + '">' + city.name + "</a>"
-  }
+    var cityLink = function(city) {
+      return '<a href="/' + city.city_code + '">' + city.name + "</a>"
+    }
 
-  $.get('/api/v1/cities', function(data){
-    var cities = _(data.cities).chain()
-    .sortBy(function(c) {
-      return c.info.user_count;
-    }).reverse()
-    .sortBy(function(c) {
-      switch(c.brew_status) {
-        case 'fully_brewed':
-          console.log(100)
-          return 100;
-        case 'warming_up':
-          return 10;
-        case 'hidden':
-          return -1000;
-      }
-      return 1;
-    })
-    .value()
+    $.get('/api/v1/cities', function(data){
+      var cities = _(data.cities).chain()
+        .sortBy(function(c) {
+          return c.info.user_count;
+        }).reverse()
+      .sortBy(function(c) {
+        switch(c.brew_status) {
+          case 'fully_brewed':
+            console.log(100)
+              return 100;
+          case 'warming_up':
+            return 10;
+          case 'hidden':
+            return -1000;
+        }
+        return 1;
+      })
+      .value()
 
-    $.each(cities, function(index, city){
+        $.each(cities, function(index, city){
 
-      console.log(city.name, city.info.user_count, city.brew_status)
-      var cityDiv = '<div class="city"><img class="city-image" src="'+city.header_bg+'"/><h3 class="city-name">' + cityLink(city) + '</h3></div>';
-      if(city.brew_status === "fully_brewed"){
-        listOfActiveCities.push(cityDiv);
-      }
-      else if(!(city.brew_status === "unapproved") && !(city.brew_status === "hidden")) {
-        listOfUpcomingCities.push(cityDiv);
+          console.log(city.name, city.info.user_count, city.brew_status)
+            var cityDiv = '<div class="city"><img class="city-image" src="'+city.header_bg+'"/><h3 class="city-name">' + cityLink(city) + '</h3></div>';
+          if(city.brew_status === "fully_brewed"){
+            listOfActiveCities.push(cityDiv);
+          }
+          else if(!(city.brew_status === "unapproved") && !(city.brew_status === "hidden")) {
+            listOfUpcomingCities.push(cityDiv);
 
-      }
+          }
+        });
+      $('.current-cities-container').html(listOfActiveCities.join(' '));
+      $('.upcoming-cities-container').html(listOfUpcomingCities.join(' '));
     });
-    $('.current-cities-container').html(listOfActiveCities.join(' '));
-    $('.upcoming-cities-container').html(listOfUpcomingCities.join(' '));
-  });
+  }
 };
 
 
