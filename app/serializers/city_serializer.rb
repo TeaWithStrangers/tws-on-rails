788ae91a1acf298727c9609238d89f947f7c60d0
mehulkar <mehul.kar@gmail.com>
2015-05-13 13:41:29 -0700

    downcase city_code for serialization, not in model

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -1,3 +1,7 @@
 class CitySerializer < ActiveModel::Serializer
   attributes :id, :name, :city_code, :timezone, :tagline, :brew_status, :description, :header_bg
+
+  def city_code
+    object.city_code.downcase if object.city_code
+  end
 end
