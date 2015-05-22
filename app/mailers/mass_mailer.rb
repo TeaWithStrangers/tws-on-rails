ed3b399922ae36647a390e0edebf812b43d573e8
mehulkar <mehul.kar@gmail.com>
2015-05-21 23:43:19 -0700

    Refactor MassMailer for readability

--- app/mailers/mass_mailer.rb
+++ app/mailers/mass_mailer.rb
@@ -5,15 +5,24 @@ class MassMailer < ActionMailer::Base
 
   FROM = '"Tea With Strangers" <sayhi@teawithstrangers.com>'
 
-  def simple_mail(opts = {})
-    from  = (opts[:from] || FROM)
-    to    = (opts[:to] || from)
-    body  = (opts[:body] || "")
-    sendgrid_recipients (opts[:recipients] || City.find(opts[:city_id]).users.select(:email).map(&:email))
 
-    mail(to: to, from: from, subject: opts[:subject]) do |f|
+  def simple_mail(opts = {})
+    from        = (opts[:from]        || FROM)
+    to          = (opts[:to]          || FROM)
+    body        = (opts[:body]        || "")
+    subject     = (opts[:subject]     || "")
+    recipients  = (opts[:recipients]  || all_recipients_for(opts[:city_id]))
+
+    sendgrid_recipients recipients
+
+    mail(to: to, from: from, subject: subject) do |f|
       f.html { render inline: markdown(body) }
       f.text { render text: body }
     end
+  end
+
+private
+  def all_recipients_for(city_id)
+    City.find(city_id).users.select(:email).map(&:email)
   end
 end
