afbd0102492acfbfa53bc92e7e328deca2a0be66
Jeremy Wong <jermspeaks@gmail.com>
2015-05-22 11:26:12 -0700

    cities.js cleanup with jshint

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,100 +1,112 @@
+'use strict';
+
+function setLoading() {
+	$('.cities-partial').addClass('loading');
+}
+
+function unsetLoading() {
+	$('.cities-partial').removeClass('loading');
+}
+
 var onCitiesIndexLoad = function() {
-  if(window.location.pathname === '/cities/' ||
-     window.location.pathname === '/cities') {
+	if (window.location.pathname === '/cities/' ||
+		window.location.pathname === '/cities') {
 
-    var listOfActiveCities = [];
-    var listOfUpcomingCities = [];
+		var listOfActiveCities = [];
+		var listOfUpcomingCities = [];
 
-    var cityLink = function(city) {
-      return '<a class="city-name" href="/' + city.city_code + '">' + city.name + "</a>"
-    }
+		var cityLink = function(city) {
+			/*jshint camelcase: false */
+			return '<a class="city-name" href="/' + city.city_code + '">' + city.name + "</a>";
+		}
 
-    var cityLinkBg = function(city) {
-      return '<a class="background-filter" href="/' + city.city_code + '">' + city.name + "</a>"
-    }
+		var cityLinkBg = function(city) {
+			/*jshint camelcase: false */
+			return '<a class="background-filter" href="/' + city.city_code + '">' + city.name + "</a>";
+		}
 
-    function setLoading() {
-      $('.cities-partial').addClass('loading');
-    }
+		setLoading();
+		$.get('/api/v1/cities', function(data) {
 
-    function unsetLoading() {
-      $('.cities-partial').removeClass('loading');
-    }
+			var cities = _(data.cities).chain()
+				.sortBy(function(c) {
+			        /*jshint camelcase: false */
+					return c.info.user_count;
+				}).reverse()
+				.sortBy(function(c) {
+					/*jshint camelcase: false */
+					switch (c.brew_status) {
+						case 'fully_brewed':
+							return 100;
+						case 'warming_up':
+							return 10;
+						case 'hidden':
+							return -1000;
+					}
+					return 1;
+				})
+				.value()
 
-    setLoading();
-    $.get('/api/v1/cities', function(data){
+			$.each(cities, function(index, city) {
+				/*jshint camelcase: false */
+				var cityDiv = '<div class="city"><div class="city-image" style="background: url(' + city.header_bg + ') no-repeat 50% 50%; background-size: cover;"><h2 class=" city-name">' + cityLinkBg(city) + cityLink(city) + '</h2></div></div>';
+				if (city.brew_status === "fully_brewed") {
+					listOfActiveCities.push(cityDiv);
+					/*jshint camelcase: false */
+				} else if (!(city.brew_status === "unapproved") && !(city.brew_status === "hidden")) {
+					listOfUpcomingCities.push(cityDiv);
 
-      var cities = _(data.cities).chain()
-        .sortBy(function(c) {
-          return c.info.user_count;
-        }).reverse()
-      .sortBy(function(c) {
-        switch(c.brew_status) {
-          case 'fully_brewed':
-              return 100;
-          case 'warming_up':
-            return 10;
-          case 'hidden':
-            return -1000;
-        }
-        return 1;
-      })
-      .value()
+				}
+			});
+			$('.current-cities-container').html(listOfActiveCities.join(' '));
+			$('.upcoming-cities-container').html(listOfUpcomingCities.join(' '));
 
-        $.each(cities, function(index, city){
-            var cityDiv = '<div class="city"><div class="city-image" style="background: url('+city.header_bg+') no-repeat 50% 50%; background-size: cover;"><h2 class=" city-name">' + cityLinkBg(city) + cityLink(city) + '</h2></div></div>';
-          if(city.brew_status === "fully_brewed"){
-            listOfActiveCities.push(cityDiv);
-          }
-          else if(!(city.brew_status === "unapproved") && !(city.brew_status === "hidden")) {
-            listOfUpcomingCities.push(cityDiv);
-
-          }
-        });
-      $('.current-cities-container').html(listOfActiveCities.join(' '));
-      $('.upcoming-cities-container').html(listOfUpcomingCities.join(' '));
-
-      unsetLoading();
-    });
-  }
+			unsetLoading();
+		});
+	}
 };
 
 var onCitiesShow = function() {
-  var leading_button = $('a[data-user-interest="leading"]');
-  var hosting_button = $('a[data-user-interest="hosting"]');
+	/*jshint camelcase: false */
+	var leading_button = $('a[data-user-interest="leading"]');
+	/*jshint camelcase: false */
+	var hosting_button = $('a[data-user-interest="hosting"]');
 
-  $.get('/api/v1/users/self/interests', function(user_info) {
-    if(user_info.leading) {
-      leading_button.addClass('applied');
-    }
+	$.get('/api/v1/users/self/interests', function(user_info) {
+		if (user_info.leading) {
+			leading_button.addClass('applied');
+		}
 
-    if(user_info.hosting) {
-      hosting_button.addClass('applied');
-    }
+		if (user_info.hosting) {
+			hosting_button.addClass('applied');
+		}
 
-  });
+	});
 
-  [leading_button, hosting_button].forEach(function(btn) {
-    btn.on('click', function(evt) {
-      var type = evt.currentTarget.dataset.userInterest;
-      var data = {'tws_interests': {}};
-      data['tws_interests'][type] = true
-        $.ajax({
-          url: '/api/v1/users/self/interests',
-          type: 'PATCH',
-          data: data,
-          success: function(d) {
-            $(evt.currentTarget).addClass('applied', 'disabled')
-          }
-        });
-    });
-  });
+	[leading_button, hosting_button].forEach(function(btn) {
+		btn.on('click', function(evt) {
+			// var type = evt.currentTarget.dataset.userInterest;
+			var data = {
+				'tws_interests': {}
+			};
+			/*jshint camelcase: false */
+			data.tws_interests.type = true;
+			$.ajax({
+				url: '/api/v1/users/self/interests',
+				type: 'PATCH',
+				data: data,
+				success: function(d) {
+					$(evt.currentTarget).addClass('applied', 'disabled')
+				}
+			});
+		});
+	});
 }
 
 
 var fns = [onCitiesIndexLoad, onCitiesShow];
 
-fns.forEach(function(e,i) {
-  $(document).ready(e)
-  $(document).on('page:load', e)
+fns.forEach(function(e) {
+	$(document).ready(e);
+	$(document).on('page:load', e);
 });
