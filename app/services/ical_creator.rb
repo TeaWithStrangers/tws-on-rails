480878c576cd4d4b92634c63d58f28dc7c3266f5
Mehul Kar <mehul.kar@apple.com>
2015-05-25 22:59:50 -0700

    Move teatime.tzid method to ical creator.
    This method doesn't work, but it is only consumed in iCalCreator.
    Moving the method there so callbacks in the model don't fail
    and fail to create the teatime

--- app/services/ical_creator.rb
+++ app/services/ical_creator.rb
@@ -8,12 +8,12 @@ class IcalCreator
   def call
     cal = Icalendar::Calendar.new
 
-    cal.add_timezone(TZInfo::Timezone.get(tt.tzid).ical_timezone(tt.start_time))
+    cal.add_timezone(TZInfo::Timezone.get(tzid).ical_timezone(tt.start_time))
 
     cal.event do |e|
       e.uid = tt.id.to_s
-      e.dtstart   = Icalendar::Values::DateTime.new tt.start_time, tzid: tt.tzid
-      e.dtend     = Icalendar::Values::DateTime.new tt.end_time, tzid: tt.tzid
+      e.dtstart   = Icalendar::Values::DateTime.new tt.start_time, tzid: tzid
+      e.dtend     = Icalendar::Values::DateTime.new tt.end_time, tzid: tzid
       e.summary   = "Tea time, hosted by #{tt.host.name}"
       e.location  = tt.location
 
@@ -22,5 +22,13 @@ class IcalCreator
     end
 
     cal
+  end
+
+  # This method used to be on the teatime model directyl
+  # but it doesn't work, so I'm putting it here (the only place
+  # it is used), so we can work on tea time model without throwing
+  # exceptions
+  def tzid
+    tt.try(:start_time).try(:time_zone).try(:tzinfo).try(:name)
   end
 end
