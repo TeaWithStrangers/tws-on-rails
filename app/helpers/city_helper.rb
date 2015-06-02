b24c2dc0982e3fbbcc3c372b3d8479844f9ac30d
Ankit <a.shah811@gmail.com>
2015-06-02 15:42:00 -0700

    new tea time schedule design

--- app/helpers/city_helper.rb
+++ app/helpers/city_helper.rb
@@ -2,4 +2,8 @@ module CityHelper
   def progress_bar(val, max)
     raw(render partial: 'cities/show/progress_bar', locals: {val: val, max: max})
   end
+
+  def tea_spots_progress_bar(val, max)
+    raw(render partial: 'cities/show/tea_spots_progress_bar', locals: {val: val, max: max})
+  end
 end
