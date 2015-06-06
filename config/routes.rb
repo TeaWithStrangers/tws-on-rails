03a0f8f9abcea9b18ae2280e5ca34a08375c5477
mehulkar <mehul.kar@gmail.com>
2015-06-05 23:55:25 -0500

    Whitespace in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -38,39 +38,34 @@ Rails.application.routes.draw do
     end
   end
 
-  # TODO: You know what to do Tue May 5 2015
   get '/cities' => 'cities#forbes_index'
   resources :cities do
     collection do
-      get '/suggest' => 'cities#forbes_new', as: :suggest
-      post '/suggest' => 'cities#forbes_create', as: :suggest_create
+      get '/suggest'  => 'cities#forbes_new',     as: :suggest
+      post '/suggest' => 'cities#forbes_create',  as: :suggest_create
     end
     member do
-      get '/host/:host_id' => 'hosts#show', as: :host
+      get '/host/:host_id' => 'hosts#show',   as: :host
       put '/set', action: 'forbes_set_city',  as: :set
     end
   end
 
-  scope(path: 'admin')  do
-    get '/ghost'           => 'admin#find'
-    post '/ghost'          => 'admin#ghost'
-
+  scope :admin  do
+    get '/ghost'              => 'admin#find'
+    post '/ghost'             => 'admin#ghost'
     get '/overview'           => 'admin#overview'
     get '/overview/tea_times' => 'admin#tea_times_overview'
     get '/overview/cities'    => 'admin#cities_overview'
     get '/overview/hosts'     => 'admin#host_overview'
     get '/users'              => 'admin#users'
-
-    get '/mail'            => 'admin#write_mail'
-    post '/mail'           => 'admin#send_mail'
-
-    get '/host'            => 'hosts#new',     as: :new_host
-    post '/host'           => 'hosts#create',  as: :create_host
+    get '/mail'               => 'admin#write_mail'
+    post '/mail'              => 'admin#send_mail'
+    get '/host'               => 'hosts#new',                 as: :new_host
+    post '/host'              => 'hosts#create',              as: :create_host
   end
 
-
-  scope(path: 'profile') do
-    get '/'        => 'profiles#show',       as: :profile, via: :get
+  scope :profile do
+    get '/'         => 'profiles#show',       as: :profile, via: :get
     get '/history'  => 'profiles#history',    as: :history
     get '/tasks'    => 'profiles#host_tasks', as: :host_tasks
   end
