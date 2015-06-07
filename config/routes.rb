946bcb3a71603e1f23a9eff7d7f65f99788eb6a1
mehulkar <mehul.kar@gmail.com>
2015-06-07 15:32:58 -0500

    Use SessionsController for all session requests.
    Since it inherits from Devise:SessionsController, we don't actually
    need to define all the controller actions, but we remove some of the
    magic by using our own code

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}, :skip => [:sessions]
   as :user do
     get     'signin'   => 'sessions#new',             as: :new_user_session
-    post    'signin'   => 'devise/sessions#create',   as: :user_session
-    delete  'signout'  => 'devise/sessions#destroy', as: :destroy_user_session
-    get     'logout'   => 'devise/sessions#destroy'
+    post    'signin'   => 'sessions#create',          as: :user_session
+    delete  'signout'  => 'sessions#destroy',         as: :destroy_user_session
+    get     'logout'   => 'sessions#destroy'
   end
 
   namespace :api do
