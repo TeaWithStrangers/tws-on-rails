TeaWithStrangers/tws-on-rails

commit 3f0d26fd400bd63f15cdb0136da9ee1ad93fcb84

Remove City#show and point it all to forbes_city_path.
Next step is to rename forbes_city_path to city_path and remove the forbes
reference

app/controllers/cities_controller.rb
@@ -21,10 +21,6 @@ class CitiesController < ApplicationController
     use_new_styles
   end
 
-  def forbes_show
-    use_new_styles
-  end
-
   def forbes_new
     use_new_styles
     if current_user
@@ -55,9 +51,9 @@ class CitiesController < ApplicationController
     redirect_to forbes_city_path(@city)
   end
 
-  # GET /cities/:city_code
+  # GET /:id (city code)
   def show
-    redirect_to forbes_city_path
+    use_new_styles
   end
 
   # GET /cities/new

app/controllers/attendance_controller.rb
@@ -27,7 +27,7 @@ class AttendanceController < ApplicationController
         "You're set for tea time! Check your email and add it to your calendar :)"
       return redirect_to profile_path, notice: message
     else
-      return redirect_to city_path(@tea_time.city), alert: "Couldn't register for that, sorry :("
+      return redirect_to forbes_city_path(@tea_time.city), alert: "Couldn't register for that, sorry :("
     end
   end
 

app/views/admin/users.html.haml
@@ -3,7 +3,7 @@
     .row
       %h1
         -if city
-          =link_to city.name, city_path(city)
+          =link_to city.name, forbes_city_path(city)
         - else
           No Home City
       %table.attendees
@@ -18,4 +18,4 @@
             %td=  a.email
             %td=  a.created_at
             %td=  a.attendances.count
-          
+

app/views/cities/_sched_no_hosts.html.haml
@@ -1,16 +1,16 @@
 .row
   .narrow.howitworks
     - if @city.hosts.empty?
-      %h3.divider 
+      %h3.divider
         = "There are no tea times happening in #{@city.name} at the moment!"
-      %p 
+      %p
         = "We're working to make sure that we have amazing hosts and enough people to ensure that tea time won't just be you and...you. If you've signed up for TWS in #{@city.name}, you'll get an email the second we're ready! If not,"
-        = link_to "sign up here!", city_path(@city), :class => "city-name #{@city.city_code}"
+        = link_to "sign up here!", forbes_city_path(@city), :class => "city-name #{@city.city_code}"
     - else
-      %h3.divider 
+      %h3.divider
         = "The #{@city.name} hosts are busy collecting stories!"
       %p= "They'll most certainly be sharing them at tea time, and when they're back, you'll find some tea times here. Check back soon!"
-    %p 
+    %p
       In the meantime, here are two things you should do:
   .wide.action
     %ul.two-things
@@ -24,7 +24,7 @@
           Enjoy stories from tea time
         %p
           = "We share some good stories from tea time, discuss some of the funny things about open communication, and share lots of behind the scenes details about Tea With Strangers on our "
-          = link_to "facebook page", "http://facebook.com/teawithstrangers" 
+          = link_to "facebook page", "http://facebook.com/teawithstrangers"
           and #{link_to 'twitter', 'http://twitter.com/teawstrangers'}.
         <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Ffacebook.com%2Fteawithstrangers&amp;width=0&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=false&amp;height=80&amp;appId=360734737286705" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
         <a href="https://twitter.com/teawstrangers" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @teawstrangers</a>

app/views/cities/show.html.haml
@@ -1,18 +1,0 @@
-- content_for :title do
-  #{@city.name} | Tea With Strangers
-- content_for :description do
-  Tea With Strangers is all about making meaningful interactions more fun. Meet some strangers in #{@city.name}!
-
-- content_for :head do
-  %meta{:property => "og:type", :content => "website"}
-  %meta{:property => "og:title", :content => "Tea With Strangers in #{@city.name}"}
-  %meta{:property => "og:image", :content => "#{@city.header_bg.url}"}
-  %meta{:property => "og:description", :content => "Tea With Strangers is all about making meaningful interactions more fun. Meet some strangers in #{@city.name}!"}
-= render partial: 'shared/city_banner'
-.container
-  .city-flash.notice
-    These old style city pages will be updated to the new style soon!
-.container{:class => "#{@city.city_code}"}
-  = render partial: 'shared/nav_header'
-  = @city.hosts.empty? ? render(partial: 'city_no_hosts') : render(partial: 'city_has_hosts')
-=content_for(:footer) { render partial: 'shared/footer' }

app/views/profiles/_user_attendance_info.html.erb
@@ -31,7 +31,7 @@
 <% elsif !current_user.home_city.nil? && attendances.count.zero? %>
   <p>
     You're not scheduled for any tea times! Let's change that.
-    <%= link_to "Go schedule a tea time!", city_path(current_user.home_city) %>
+    <%= link_to "Go schedule a tea time!", forbes_city_path(current_user.home_city) %>
   </p>
 <% else %>
   <p>

app/views/shared/_signup.html.haml
@@ -14,6 +14,6 @@
   .already
     %h4.schedule.button
       - if @city
-        =link_to("Schedule tea time", city_path(@city))
+        =link_to("Schedule tea time", forbes_city_path(@city))
       -else
-        =link_to("Schedule tea time", city_path(current_user.home_city))
+        =link_to("Schedule tea time", forbes_city_path(current_user.home_city))

commit 75cfb54fa10c2875874bf27c44be48cc405530e0

Unauthenticated users should be able to see city pages.
This regression was introduced in 3f0d26f.
Closes #474

app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
   before_action :set_city, except: [:index, :new, :index]
   before_action :authenticate_user!, :authorized?, only: [:new, :create, :edit, :update, :destroy]
-  before_action :away_ye_waitlisted, except: [:index, :forbes_show, :forbes_set_city]
+  before_action :away_ye_waitlisted, except: [:index, :show, :forbes_set_city]
 
   # GET /cities
   def index

spec/features/city_page_spec.rb
@@ -1,0 +1,11 @@
+require 'spec_helper'
+
+feature 'City page' do
+  scenario 'Unauthenticated user' do
+    city = create(:city, name: "Bahamas", city_code: "XY")
+    visit forbes_city_path(city)
+    expect(page).to have_text("Bahamas")
+    expect(page).not_to have_css('flash alert')
+    expect(page.current_path).to eq "/XY"
+  end
+end