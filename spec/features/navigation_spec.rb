899f52927859f08d4a5428bbdacab8c2ea2a735d
mehulkar <mehul.kar@gmail.com>
2015-07-20 21:20:17 -0700

    Basic interface to allow hosts to create/update email reminders

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -6,3 +6,18 @@ feature 'Header navigation links' do
     expect(page.find('header nav')).to have_link('Hosting')
   end
 end
+
+feature 'Profile navigation links' do
+  scenario 'host' do
+    host = create(:user, :host)
+    sign_in(host)
+    visit profile_path
+    expect(page.find('#profile-nav')).to have_link('Customize Reminder Email')
+  end
+  scenario 'As non host' do
+    user = create(:user)
+    sign_in(user)
+    visit profile_path
+    expect(page.find('#profile-nav')).not_to have_link('Customize Reminder Email')
+  end
+end
