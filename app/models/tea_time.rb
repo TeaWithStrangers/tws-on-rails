3e7be125369c6469750c27bb24a3be49cf78084d
mehulkar <mehul.kar@gmail.com>
2015-07-20 19:12:14 -0700

    Split after_commit callbacks on Attendance model for readability

--- app/models/tea_time.rb
+++ app/models/tea_time.rb
@@ -13,7 +13,10 @@ class TeaTime < ActiveRecord::Base
   accepts_nested_attributes_for :attendances
 
   after_touch :clear_association_cache_wrapper
-  after_commit :send_host_confirmation, :queue_reminder_to_mark_attendance, on: :create, unless: :skip_callbacks
+
+  after_commit :queue_reminder_to_mark_attendance,  on: :create, unless: :skip_callbacks
+  after_commit :send_host_confirmation,             on: :create, unless: :skip_callbacks
+
   before_destroy { CancelTeaTime.send_cancellations(self) }
 
   enum followup_status: { pending: 0, marked_attendance: 1, completed: 2, cancelled: 3 }
