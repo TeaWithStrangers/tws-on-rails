6203372712201b5eb3e8181e6c0cff98099dd052
mehulkar <mehul.kar@gmail.com>
2015-07-27 19:34:03 -0700

    Use makerb to render custom templates.
    This allows us to render text and html templates in the
    same way as all the other emails

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -29,18 +29,11 @@ class AttendanceMailer < ActionMailer::Base
     cancel_delivery unless @attendance
 
     @user = @attendance.user
-    tt = @attendance.tea_time
+    @tt = @attendance.tea_time
 
-    body = tt.host.email_reminder.body
+    @body = @tt.host.email_reminder.body
 
-    cancel_delivery unless body
-
-    body += "\n\n <hr> *Note from the Robots: This email is about your tea time on #{tt.date_to_email}. It's at #{tt.location}. Enjoy it!*"
-
-    # See here for options https://github.com/vmg/redcarpet
-    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true, escape_html: true)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    sanitized_body = markdown.render(body)
+    cancel_delivery unless @body
 
     attachments['event.ics'] = {
       mime_type: "text/calendar",
@@ -51,10 +44,12 @@ class AttendanceMailer < ActionMailer::Base
 
     mail(
       to:           @attendance.user.email,
-      from:         "\"#{tt.host.nickname} at Tea With Strangers\" <#{tt.host.email}>",
+      from:         "\"#{@tt.host.nickname} at Tea With Strangers\" <#{@tt.host.email}>",
       subject:      "Your tea time is coming up!",
-      body:         sanitized_body,
-      content_type: "text/html")
+    ) do |format|
+      format.text
+      format.html
+    end
   end
 
   # Twelve-hour and two day reminder of a tea time a user is attending
