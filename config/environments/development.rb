5592645823667e98a40917076ec3c70354e7c293
mehulkar <mehul.kar@gmail.com>
2015-07-27 20:14:14 -0700

    Set host option to localhost:3000 for action mailer in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  config.action_mailer.default_url_options = {host: 'http://localhost' }
+  config.action_mailer.default_url_options = {host: 'http://localhost:3000' }
 
   # Background Worker Config
   # Run DJ jobs synchronously in development
